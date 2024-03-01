f66419d49baeba24c7905e25948ee379f9541a69
Muhammad Ammar Shaikh <117350592+Ammar-Shaikh-00@users.noreply.github.com>
2024-03-02 01:19:17 +0500

    Qn_2.ts

--- Qn_2.ts
+++ Qn_2.ts
@@ -1,4 +1,12 @@
-let Name:string = ("Ammar")
-console.log (Name.toLowerCase)
-console.log (Name.toUpperCase)
-console.log (Name.toLocaleUpperCase)
+let Name: string = ("Ammar")
+
+console.log(Name.toLowerCase());
+console.log(Name.toUpperCase());
+
+// we create a function to convert a string to title case
+function toTitleCase(str: string): string {
+    return str.replace(/\w\S*/g, (txt) => {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
+console.log(toTitleCase(Name));
